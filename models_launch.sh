7c586a806a20ab6d3a7c08dc74dbe9e716161d13
AxelleSchyns <schyns.axelle@gmail.com>
2023-06-04 22:22:16 +0200

    retr images

--- models_launch.sh
+++ models_launch.sh
@@ -17,18 +17,18 @@
 #python database/retrieve_images.py --path /home/labarvr4090/Documents/Axelle/cytomine/Data/validation/janowczyk1_1/8867_116795971_0_0_250_250.png --extractor resnet --weights weights_folder/50_resnet --results_dir /home/labarvr4090/Documents/Axelle/cytomine/cbir-tfe/results/40_resnet --generalise 3
 
 
-printf "116 byol \n"
-python database/models.py --model byol  --weights weights_folder/116_byol --training_data /home/labarvr4090/Documents/Axelle/cytomine/Data/train --scheduler exponential  --num_epochs 15 --generalise 2 --batch_size 64
+#printf "116 byol \n"
+#python database/models.py --model byol  --weights weights_folder/116_byol --training_data /home/labarvr4090/Documents/Axelle/cytomine/Data/train --scheduler exponential  --num_epochs 15 --generalise 2 --batch_size 64
 
 
-#printf "116 byol + test\n"
+printf "116 byol + test\n"
 #python database/add_images.py --path /home/labarvr4090/Documents/Axelle/cytomine/Data/test --extractor byol --weights weights_folder/116_byol --rewrite --num_features 256 --generalise 2
 
 #printf "116 byol: all \n"
-#python database/test_accuracy.py --path /home/labarvr4090/Documents/Axelle/cytomine/Data/validation --extractor byol --weights weights_folder/116_byol --measure all --num_features 256 -
+#python database/test_accuracy.py --path /home/labarvr4090/Documents/Axelle/cytomine/Data/validation --extractor byol --weights weights_folder/116_byol --measure all --num_features 256 
 
 #printf "116 byol: weighted \n"
-#python database/test_accuracy.py --path /home/labarvr4090/Documents/Axelle/cytomine/Data/validation --extractor byol --weights weights_folder/116_byol --measure weighted --num_features 256 --generalise 2
+python database/test_accuracy.py --path /home/labarvr4090/Documents/Axelle/cytomine/Data/validation --extractor byol --weights weights_folder/116_byol --measure weighted --num_features 256 --generalise 2
 
 
 
